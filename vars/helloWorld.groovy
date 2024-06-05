3d714c9bcc04db097bb0a4cef65228d62b7023d1
mroluyomiosh <98926230+mroluyomiosh@users.noreply.github.com>
2024-06-05 16:59:34 -0400

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,5 +1,6 @@
+buildNode = "${config.BUILD_NODE}"
 def call(Map config = [:]) {
-  node() {
+  node(buildNode) {
     stage('Example') {
      sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
     }
