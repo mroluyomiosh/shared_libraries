18bbbdfa441ff9b62f348c9a17d62d01ecac4a85
mroluyomiosh <98926230+mroluyomiosh@users.noreply.github.com>
2024-06-06 16:03:20 -0400

    Update createBranchAndModifyFiles.groovy

--- vars/createBranchAndModifyFiles.groovy
+++ vars/createBranchAndModifyFiles.groovy
@@ -13,7 +13,10 @@ def call(Map params) {
 
         stage('Prepare Workspace') {
             // Clean the workspace directory
+            sh 'echo "Current workspace contents:"'
+            sh 'ls -la ${WORKSPACE}'
             sh 'rm -rf ${WORKSPACE}/*'
+            sh 'ls -la ${WORKSPACE}'
         }
 
         stage('Checkout') {
