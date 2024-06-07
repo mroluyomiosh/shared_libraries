e2982e170ec5249272f07532d032dfc50a48e030
mroluyomiosh <98926230+mroluyomiosh@users.noreply.github.com>
2024-06-07 16:34:10 -0400

    Update createBranchAndModifyFiles.groovy

--- vars/createBranchAndModifyFiles.groovy
+++ vars/createBranchAndModifyFiles.groovy
@@ -70,5 +70,24 @@ def call(Map params) {
                 """
             }
         }
+        stage('Check Pull Request Status') {
+            // Check if the pull request has been merged
+            def prNumber = withCredentials([gitUsernamePassword(credentialsId: 'github-token', gitToolName: 'Default')]) { sh(script: """
+                curl -H "Authorization: token ${GIT_PASSWORD}" -H "Content-Type: application/json" \
+                https://api.github.com/repos/${repoUrl.split('/')[3]}/${repoUrl.split('/')[4].replace('.git', '')}/pulls | \
+                jq '.[] | select(.head.ref=="${branchName}") | .number' | head -1
+            """, returnStdout: true).trim() }
+
+            def prMerged = withCredentials([gitUsernamePassword(credentialsId: 'github-token', gitToolName: 'Default')]) { sh(script: """
+                curl -H "Authorization: token ${GIT_PASSWORD}" -H "Content-Type: application/json" \
+                https://api.github.com/repos/${repoUrl.split('/')[3]}/${repoUrl.split('/')[4].replace('.git', '')}/pulls/${prNumber}/merge
+            """, returnStatus: true) == 204 }
+
+            if (prMerged) {
+                echo "Pull request #${prNumber} has been merged."
+            } else {
+                error "Pull request #${prNumber} has not been merged yet."
+            }
+        }
     }
 }
