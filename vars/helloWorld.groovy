e4dd9708e95252e2694e9047e99347b0a98618c5
mroluyomiosh <98926230+mroluyomiosh@users.noreply.github.com>
2024-06-07 16:03:21 -0400

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,7 +1,74 @@
-def call(Map config = [:]) {
-  node() {
-    stage('Example') {
-     sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
+// vars/modifyAndCommitFile.groovy
+import groovy.json.JsonOutput
+
+def call(Map params) {
+    node {
+        def repoUrl = params.REPO_URL
+        def originalFile = params.ORIGINAL_FILE
+        def newFileName = params.NEW_FILE_NAME
+        def productName = params.PRODUCT_NAME
+        def applicationName = params.APPLICATION_NAME
+        def testApplicationName = params.TEST_APPLICATION_NAME
+        def branchName = params.BRANCH_NAME
+
+        stage('Prepare Workspace') {
+            // Clean the workspace directory
+            deleteDir()
+        }
+
+        stage('Checkout') {
+            // Clone the repository
+            sh """
+                git config --global user.email "mroluyomiosh@gmail.com"
+                git config --global user.name "mroluyomiosh"
+                git clone ${repoUrl} .
+                git checkout main
+            """
+        }
+        
+        stage('Create Feature Branch') {
+            // Create a new feature branch
+            sh "git checkout -b ${branchName}"
+        }
+        
+        stage('Copy and Modify File') {
+            // Copy and rename the file
+            sh "cp ${originalFile} ${newFileName}"
+
+            // Modify the new file
+            sh """
+                sed -i 's/ProductName="abc"/ProductName="${productName}"/' ${newFileName}
+                sed -i 's/ApplicationName="xyz"/ApplicationName="${applicationName}"/' ${newFileName}
+                sed -i 's/TestApplicationName="uvw"/TestApplicationName="${testApplicationName}"/' ${newFileName}
+            """
+        }
+        
+        stage('Commit and Push Changes') {
+            // Add, commit, and push changes
+            withCredentials([gitUsernamePassword(credentialsId: 'github-token', gitToolName: 'Default')]) {
+                sh """
+                    git add ${newFileName}
+                    git commit -m 'Modified ${newFileName} with new parameters'
+                    git push origin ${branchName}
+                """
+            }
+        }
+        
+        stage('Create Pull Request') {
+            // Create a pull request using GitHub API
+            def payload = JsonOutput.toJson([
+                title: "Feature: Modify ${newFileName}",
+                body: "This pull request modifies the ${newFileName} with new parameters.",
+                head: branchName,
+                base: "main"
+            ])
+            
+            withCredentials([gitUsernamePassword(credentialsId: 'github-token', gitToolName: 'Default')]) {
+                sh """
+                    curl -X POST -H "Authorization: token ${GIT_PASSWORD}" -H "Content-Type: application/json" \
+                    -d '${payload}' https://api.github.com/repos/${repoUrl.split('/')[3]}/${repoUrl.split('/')[4].replace('.git', '')}/pulls
+                """
+            }
+        }
     }
-  }
 }
