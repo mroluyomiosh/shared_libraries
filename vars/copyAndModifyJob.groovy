261a6e9c8be1fa9a2ae2aadd8f9c0eb70a87850e
mroluyomiosh <98926230+mroluyomiosh@users.noreply.github.com>
2024-06-07 11:14:17 -0400

    Update copyAndModifyJob.groovy

--- vars/copyAndModifyJob.groovy
+++ vars/copyAndModifyJob.groovy
@@ -11,35 +11,35 @@ def call(Map params) {
             // Use Jenkins REST API to copy job
             withCredentials([string(credentialsId: 'jenkins-api-token', variable: 'JENKINS_TOKEN')]) {
                 def jenkinsUrl = env.JENKINS_URL
-                def auth = "${env.JENKINS_USER}:${env.JENKINS_TOKEN}"
+                def auth = '${env.JENKINS_USER}:${env.JENKINS_TOKEN}'
 
-                sh """
+                sh '''
                     curl -X POST -u ${auth} ${jenkinsUrl}/createItem?name=${targetJob}&mode=copy&from=${sourceJob}
-                """
+                '''
             }
         }
 
-        stage('Modify Job') {
-            echo "Modifying job ${targetJob}..."
-            // Get the job configuration using Jenkins REST API
-            withCredentials([string(credentialsId: 'jenkins-api-token', variable: 'JENKINS_TOKEN')]) {
-                def auth = "${env.JENKINS_USER}:${env.JENKINS_TOKEN}"
+        // stage('Modify Job') {
+        //     echo "Modifying job ${targetJob}..."
+        //     // Get the job configuration using Jenkins REST API
+        //     withCredentials([string(credentialsId: 'jenkins-api-token', variable: 'JENKINS_TOKEN')]) {
+        //         def auth = '${env.JENKINS_USER}:${env.JENKINS_TOKEN}'
 
-                def jobConfig = sh(script: """
-                    curl -s -u ${auth} ${env.JENKINS_URL}/job/${targetJob}/config.xml
-                """, returnStdout: true).trim()
+        //         def jobConfig = sh(script: '''
+        //             curl -s -u ${auth} ${env.JENKINS_URL}/job/${targetJob}/config.xml
+        //         ''', returnStdout: true).trim()
 
-                // Replace parameters in jobConfig
-                parameters.each { key, value ->
-                    jobConfig = jobConfig.replaceAll(key, value)
-                }
+        //         // Replace parameters in jobConfig
+        //         parameters.each { key, value ->
+        //             jobConfig = jobConfig.replaceAll(key, value)
+        //         }
 
-                // Update job configuration using Jenkins REST API
-                writeFile(file: 'job-config.xml', text: jobConfig)
-                sh """
-                    curl -X POST -u ${auth} -H "Content-Type: application/xml" --data-binary @job-config.xml ${env.JENKINS_URL}/job/${targetJob}/config.xml
-                """
-            }
-        }
+        //         // Update job configuration using Jenkins REST API
+        //         writeFile(file: 'job-config.xml', text: jobConfig)
+        //         sh '''
+        //             curl -X POST -u ${auth} -H "Content-Type: application/xml" --data-binary @job-config.xml ${env.JENKINS_URL}/job/${targetJob}/config.xml
+        //         '''
+        //     }
+        // }
     }
 }
